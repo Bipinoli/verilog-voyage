af53d7cf777673e0d97d24caaa7e948afcf758f1
boli <bipinoli90@gmail.com>
2024-05-21 18:57:12 +0200

    display waveforms with gtkwave

---
+++ first/display_waveforms.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gtkwave example.vcd
--- first/example_test.v
+++ first/example_test.v
@@ -4,6 +4,8 @@ module testbench;
 
   initial 
   begin
+    $dumpfile ("example.vcd");
+    $dumpvars (0, testbench);
     $monitor ($time, " A=%b, B=%b, C=%b, D=%b, E=%b, F=%b, Y=%b", A,B,C,D,E,F,Y);
     #5 A=1; B=0; C=0; D=1; E=0; F=0;
     #5 A=0; B=0; C=1; D=1; E=0; F=0;
